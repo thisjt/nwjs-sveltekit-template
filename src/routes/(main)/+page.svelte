b7d0a61a00d95271b5f25affa83e357199ca49e7
Joshua Tahanlangit <contact@thisjt.me>
2024-10-01 01:53:42 +0800

    feat: separate adapters for building

---
+++ src/routes/(main)/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { onMount } from 'svelte';
+	let hello = 'sad';
+
+	onMount(() => {
+		fetch('/api')
+			.then((data) => data.json())
+			.then((data) => {
+				hello = data.hello;
+			});
+	});
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Hello {hello}</p>
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
+import { default as adapterNode } from '@sveltejs/adapter-node';
+import { default as adapterStatic } from '@sveltejs/adapter-static';
+import 'dotenv/config';
+
+const NWJS_BUILD_STATIC = process.env.NWJS_BUILD_STATIC;
+const NWJS_BUILD_API = process.env.NWJS_BUILD_API;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,7 +11,14 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: NWJS_BUILD_STATIC ? adapterStatic() : adapterNode(),
+		files: {
+			routes: NWJS_BUILD_STATIC
+				? 'src/routes/(main)'
+				: NWJS_BUILD_API
+					? 'src/routes/(api)'
+					: 'src/routes'
+		}
 	}
 };
 
