b25ae771a0ef137256d2658da5b350a7b242e4f0
Joshua Tahanlangit <contact@thisjt.me>
2024-10-01 17:16:52 +0800

    refactor: use private and public envs in api

--- src/routes/(api)/api/+server.js
+++ src/routes/(api)/api/+server.js
@@ -1,5 +1,7 @@
 import { json } from '@sveltejs/kit';
+import { PRIVATE_STRING } from '$env/static/private';
+import { PUBLIC_STRING } from '$env/static/public';
 
 export async function GET() {
-	return json({ hello: 'world' });
+	return json({ hello: 'world', PUBLIC_STRING, PRIVATE_STRING });
 }
